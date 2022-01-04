c01717d6f99e4b2fec770ee19d357273cf32d3ec
felipehads <felipehads@outlook.com>
2022-01-04 10:44:54 -0300

    Utilidades das gerações com plot de gráfico

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@
 #Mutação:
 #Método da Roleta:
 
-#Procedimento AG{ 
+#Procedimento AG { 
 #   t = 0;
 #   inicia_população (P, t)
 #   avaliação (P, t);
@@ -18,82 +18,64 @@
 #     sobrevivem (P, t)
 #   }
 #}
-import numpy as np
 from random import random, randint, getrandbits
 
-# Geração de item, com 0(ausência) e 1(presença)
+# Geração de combinação de itens individual, com 0(ausência) e 1(presença)
 def itemCombination(number_of_items):
   return [ getrandbits(1) for x in range (number_of_items) ]
 
-# Geração da população dos items
+# Geração da população dos itens
 def generatePopulation(number_of_combinations, number_of_items):
   return [ itemCombination(number_of_items) for x in range(number_of_combinations) ]
 
-# Função de avaliação realizada em cada item
-def fitness(items, max_weight, max_cost, list_of_items):
+# Função de avaliação da utilidade da combinação de itens, só retorna a utilidade caso passe pelas restrições:
+# Restrições: limite de 30kg (weight) / limite de R$100,00 (cost)
+def fitness(item, max_weight, max_cost, list_of_items):
   total_weight, total_cost, total_value = 0, 0, 0
 
-  for index, value in enumerate(items):
-    total_weight += (items[index] * list_of_items[index][0])
-    total_cost += (items[index] * list_of_items[index][1])
-    total_value += (items[index] * list_of_items[index][2])
+  for index, value in enumerate(item):
+    total_weight += (item[index] * list_of_items[index][0])
+    total_cost += (item[index] * list_of_items[index][1])
+    total_value += (item[index] * list_of_items[index][2])
   
   if ((total_weight > max_weight) or (total_cost > max_cost)):
     return -1
 
   return total_value
 
-# Função que, através dos valores obtidos com a função anterior, retorna a média destes valores 
+# Função que, através dos valores obtidos com a função anterior, retorna a média das utilidades em certa geração
 def average_fitness(items, max_weight, max_cost, list_of_items):
-  # totalValue = 0
   
-  # for item in items:
-  #   if (fitness(item, max_weight, max_cost, list_of_items) >= 0):
-  #     totalValue += fitness(item, max_weight, max_cost, list_of_items)
-
   sum_of_valid_fitness = sum(fitness(item, max_weight, max_cost, list_of_items) for item in items if fitness(item, max_weight, max_cost, list_of_items) >= 0)
   
   return sum_of_valid_fitness / (len(items))
 
+# Função utilizada no processo evolutivo para selecionar os pais sorteados
 def parents_selection(parents):
 
   # [(29, 34, 57, 10) , ('010100', '100100', '101010', '111001')] 
   values = list(zip(*parents))  
-  # print(values)
   
   total_fitness = sum(values[0])
 
-  # roulette, accumulation, sorted_value = [] , 0, random.uniform(0, total_fitness)
 
   def sort_index(total_fitness, last_index_selected = -1):
+    roulette, accumulation, sorted_value = [] , 0, random()
 
     if (last_index_selected != -1):
-      total_fitness -= parents[0][last_index_selected]
+      total_fitness -= values[0][last_index_selected]
 
-    selection_probs = [(value/total_fitness) for value in values[0]]
+    for index, fitness_value in enumerate(values[0]):
 
-  # for index, fitness_value in enumerate(values[0]):
+      if (last_index_selected == index):
+        continue
 
-    result = np.random.choice(values[1], p = selection_probs) 
-    index = values[1].index(result)
+      accumulation += fitness_value
 
-    while (index == last_index_selected):
-      result = np.random.choice(values[1], p = selection_probs) 
-      index = values[1].index(result)
+      roulette.append(accumulation/total_fitness)
 
-    return values[1].index(result)
-
-    # if (last_index_selected == index):
-    #   continue
-
-    # accumulation += fitness_value
-
-    # roulette.append(accumulation)
-
-
-    # if(roulette[index] >= sorted_value):
-    #   return index
-
+      if(roulette[-1] >= sorted_value):
+        return index
 
   father_index = sort_index(total_fitness)
   mother_index = sort_index(total_fitness, father_index)
@@ -103,34 +85,33 @@ def parents_selection(parents):
 
   return father, mother
 
-# Função evolutiva
+# Função evolutiva: realizar o cruzamento e mutação da população
 def evolve(items, max_weight, max_cost, list_of_items, number_of_combinations, mutate_percentage = 0.05):
-  # Mapeia os fitness com seus valores binários
-  parents = [[item] for item in items if fitness() ] 
 
-  # print(parents)
+  # Mapeia os fitness com seus valores binários
+  parents = [[fitness(item, max_weight, max_cost, list_of_items), item] for item in items if fitness(item, max_weight, max_cost, list_of_items) >= 0 ] 
 
   parents.sort(reverse=True)
 
   sons = []
 
   # Reprodução/Escolha dos pais
-  # while len(sons) < number_of_combinations:
-  #   father, mother = parents_selection(parents)
+  while len(sons) < number_of_combinations:
+    father, mother = parents_selection(parents)
 
-  #   middle = len(father) // 2
-  #   son = father[:middle] + mother[middle:]
-  #   sons.append(son)
+    middle = len(father) // 2
+    son = father[:middle] + mother[middle:]
+    sons.append(son)
 
-  # # Mutação
-  # for son in sons:
-  #   if (mutate_percentage > random()):
-  #     mutate_position = randint(0, len(son) -1)
-  #     if (son[mutate_position] == 1):
-  #       son[mutate_position] = 0
-  #     else:
-  #       son[mutate_position] = 1
+  # Mutação
+  for son in sons:
+    if (mutate_percentage > random()):
+      mutate_position = randint(0, len(son) -1)
+      if (son[mutate_position] == 1):
+        son[mutate_position] = 0
+      else:
+        son[mutate_position] = 1
 
-  # return sons
+  return sons
 
   
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions import *
-
+from matplotlib import pyplot
 
 # [(29, 34, 57, 10) , ('010100', '100100', '101010', '111001')]
 list_of_items = [[5,10,1], [2,15,1], [7,2,3], [7,5,1], [6,15,2], [2,16,4], [3,2,4], [1,8,2], [2,7,5], [3,15,1], [4,16,1], [7,2,5], [1,14,1], [1,13,5], [4,8,2], [8,5,2], [5,1,2], [8,13,5], [2,20,3], [4,9,3], [6,5,1], [8,4,1], [5,12,5], [4,7,4], [7,11,2], [8,17,4], [8,1,3], [6,10,4], [1,11,4], [3,6,5]]
@@ -7,27 +7,26 @@ list_of_items = [[5,10,1], [2,15,1], [7,2,3], [7,5,1], [6,15,2], [2,16,4], [3,2,
 max_weight = 30
 max_cost = 100
 
-number_of_combinations = 5
+number_of_combinations = 2000
 generations = 80
 number_of_items = len(list_of_items)
-# print(number_of_items)
 
-# print(itemCombination(30))
-# [0, [1,1,0,0,1]]
-# [1, [0,0,1,0,1]]
 items = generatePopulation(number_of_combinations, number_of_items)
-# for item in enumerate(items):
-#   print(item)
-# print(items)
+
 fitness_history = [average_fitness(items, max_weight, max_cost, list_of_items)]
 
-# print(fitness_history)
-
-# average_fitness()
-
 for i in range(generations):
-  evolve(items, max_weight, max_cost, list_of_items, number_of_combinations)
+  items = evolve(items, max_weight, max_cost, list_of_items, number_of_combinations)
   fitness_history.append(average_fitness(items, max_weight, max_cost, list_of_items))
 
-# for index, data in enumerate(fitness_history):
-#   print("Generation: ", index, " | Average value:", data)
+# IMPRIME AS MÉDIAS DE UTILIDADE POR GERAÇÃO
+for index, data in enumerate(fitness_history):
+  print("Generation: ", index, " | Average value:", data)
+
+# PLOTA O GRÁFICO DA EVOLUÇÃO DAS UTILIDADES COM O PASSAR DAS GERAÇÕES
+pyplot.plot(range(len(fitness_history)), fitness_history)
+pyplot.grid(True, zorder=0)
+pyplot.title("Trabalho Algoritmo Genético")
+pyplot.xlabel("Geracao")
+pyplot.ylabel("Utilidade media")
+pyplot.show()
